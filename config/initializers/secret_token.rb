a45932e597a47d21c1e4c1c5c0c4b8e9d6ae1430
Pavel Bezpalov <pavel.bezpalov@cyber-wizard.com>
2015-10-30 10:33:25 +0200

    Staring demo app from railstutotial 4 book

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rtapp::Application.config.secret_key_base = '5cec4639fcc0692c8f646d78f678bccdeee6f15cb9fbad81648ab1e79c8eadac614af511f9f4d303066ec13da8595b8f4abd40e7d1b4eeae46a3ce516b32de7e'
